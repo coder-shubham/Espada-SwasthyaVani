b71330bb9059744d216c3a1fd1db2d65390de088
Shubham Sarraf <shubham.sarraf@nextiva.com>
2025-04-26 08:53:38 +0530

    Add Controllers and Config

---
+++ backend/swasthyavani/src/main/java/com/espada/swasthyavani/config/WebSocketConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 Nextiva, Inc. to Present.
+ * All rights reserved.
+ */
+
+package com.espada.swasthyavani.config;
+
+/**
+ * Class Description goes here.
+ * Created by shubhamsarraf on 26/04/25
+ */
+public class WebSocketConfig {
+}
