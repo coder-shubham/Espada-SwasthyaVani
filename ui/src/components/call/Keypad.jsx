76ccfb1ffc309cf8f303801687d68a56a5630bf8
Sai Kiran Bade <saikiranbade@Sais-MacBook-Air.local>
2025-04-26 12:29:40 +0530

    Implement call UI

---
+++ ui/src/components/call/Keypad.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Keypad = ({ onKeyPress }) => {
+  const buttons = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['*', '0', '#']
+  ];
+
+  return (
+    <div className="keypad-container">
+      {buttons.map((row, rowIndex) => (
+        <React.Fragment key={rowIndex}>
+          {row.map((digit) => (
+            <button
+              key={digit}
+              className="keypad-button"
+              onClick={() => onKeyPress(digit)}
+            >
+              {digit}
+            </button>
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default Keypad;
