2077290c58734f793bf1324347ccc1dd9aee6a24
shkamboj <iiitu16118@gmail.com>
2025-05-02 18:21:07 +0530

    Add handler for stt from e2e

---
+++ ai/utils/stt/e2e/whisper.py
@@ -0,0 +1,25 @@
+
+import sys
+sys.path.append('.')
+from factory.config import HINDI, ENGLISH, FactoryConfig
+
+
+def get_text(file_path, language):
+    codes = {
+        HINDI: 'Hindi',
+        ENGLISH: 'English'
+    }
+    data = {"input": file_path,
+            "language": codes.get(language),
+            "task": "transcribe",
+            "max_new_tokens": 420,
+            "return_timestamps": "none"
+    }
+
+    response = FactoryConfig.tir_client.infer(model_name="whisper-large-v3", data=data)
+    print(response)
+    if response and len(response) >=2 and response[0] == True:
+        try:
+            return response[1].outputs[0].data[0].strip()
+        except:
+            return ''
