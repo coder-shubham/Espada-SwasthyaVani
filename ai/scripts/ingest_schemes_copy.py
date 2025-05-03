4523d28a7a45726c5113272cf18e97500a8062b2
shkamboj <iiitu16118@gmail.com>
2025-05-03 19:53:27 +0530

    Add copy of ingest schemes which can be ran directly inside docker container

---
+++ ai/scripts/ingest_schemes_copy.py
@@ -0,0 +1,68 @@
+### we can run this directly from inside of docker container
+
+import sys
+import os
+import weaviate
+from weaviate.classes.init import Auth
+from sentence_transformers import SentenceTransformer
+
+sys.path.append('.')
+
+
+from utils.vectorstores.weav8 import WeaviateCollectionClient
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PlaywrightURLLoader
+
+def _get_vector_db_client():
+    weaviate_api_key = os.getenv('WEAVIATE_API_KEY')
+    return weaviate.connect_to_local(auth_credentials=Auth.api_key(weaviate_api_key))
+
+vector_db_client = _get_vector_db_client()
+embeddings = SentenceTransformer('intfloat/multilingual-e5-large')
+
+GOV_SCHEMES_COLL_NAME = 'gov_schemes'
+GOV_SCHEMES = {
+    "Free Motorized Tricycle Scheme for Persons with Disabilities - Uttar Pradesh": "https://www.myscheme.gov.in/schemes/fmtsfpwd",
+    "Surakshit Matritva Aashwasan Yojana - India": "https://www.myscheme.gov.in/schemes/suman",
+    "Janani Shishu Suraksha Karyakram - India": "https://www.myscheme.gov.in/schemes/jssk",
+    "Medical Assistance for the Treatment of Critical Illness - Maharashtra": "https://www.myscheme.gov.in/schemes/matci",
+    "Employee Health Scheme - Andhra Pradesh": "https://www.myscheme.gov.in/schemes/ehsap",
+    "Dr. YSR Aarogyasri Health Insurance Scheme - Andhra Pradesh": "https://www.myscheme.gov.in/schemes/dysrhis",
+    "Deen Dayal Upadhyaya Antyodaya Parivar Suraksha Yojana (DAYALU) - Haryana": "https://www.myscheme.gov.in/schemes/dayalu",
+    "Ayushman Bharat - Pradhan Mantri Jan Arogya Yojana - India": "https://www.myscheme.gov.in/schemes/ab-pmjay"
+}
+
+
+def extract_page_text(url):
+    loader = PlaywrightURLLoader(urls=[url])
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=400)
+    splits = text_splitter.split_documents(docs)
+    return splits
+
+coll = WeaviateCollectionClient(db_client=vector_db_client, name=GOV_SCHEMES_COLL_NAME, embeddings=embeddings)
+coll.load_collection()
+coll.delete_collection()
+
+coll.load_collection()
+
+for scheme_name, url in GOV_SCHEMES.items():
+    docs = extract_page_text(url)
+    
+    for doc in docs:
+        properties = doc.metadata
+        properties['text'] = scheme_name + '\n\n ' + doc.page_content
+        vector = embeddings.encode(properties['text']).tolist()
+        coll.insert(properties=properties, vector=vector)
+    
+    print(f"Ingestion completed for Scheme({scheme_name})")
+
+
+print("Whole ingestion process is completed")
+
+vector_db_client.close()
+
+
+
+
